b8bec5bb31e1abed04869b408f0ca7ece4e97de6
rohmanalka <rohmanalka06@gmail.com>
2023-10-05 15:11:21 +0700

    penambahan kelipatan 400

---
+++ Pemilihan2Percobaan121.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan121 {
+    public static void main(String[] args) {
+        Scanner input21 = new Scanner(System.in);
+        System.out.print("Masukkan Tahun: ");
+        int tahun = input21.nextInt();
+
+        if ((tahun % 4)== 0){
+            if ((tahun % 100)!= 0){
+                if ((tahun % 400)== 0){
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            }
+        }
+    }
+}
