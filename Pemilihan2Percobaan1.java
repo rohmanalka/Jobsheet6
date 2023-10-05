c3e3758a5f89d9b9426bea6e4588d9339d53311a
rohmanalka <rohmanalka06@gmail.com>
2023-10-05 09:14:37 +0700

    modifikasi code pertama

--- Pemilihan2Percobaan1.java
+++ Pemilihan2Percobaan1.java
@@ -12,8 +12,8 @@ public class Pemilihan2Percobaan1 {
         if ((tahun % 4)== 0) {
             if ((tahun % 100) != 0)
                 System.out.println("Tahun Kabisat");
-        
-        }else 
+        else 
             System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
