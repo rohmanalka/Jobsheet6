a77a40cd967d67800338c5efa21ed119967ed32d
rohmanalka <rohmanalka06@gmail.com>
2023-10-05 09:57:49 +0700

    perubahan kode Bukan Tahun Kabisat

--- Pemilihan2Percobaan1.java
+++ Pemilihan2Percobaan1.java
@@ -9,11 +9,12 @@ public class Pemilihan2Percobaan1 {
         System.out.print("Masukkan Tahun: ");
         int tahun = input21.nextInt();
 
-        if ((tahun % 4)== 0) {
-            if ((tahun % 100) != 0)
-                System.out.println("Tahun Kabisat");
-        else 
+        if ((tahun % 4)== 0){
+            if ((tahun % 100)!= 0){
+            System.out.println("Tahun Kabisat");
+        } else {
             System.out.println("Bukan Tahun Kabisat");
+            }
         }
     }
 }
