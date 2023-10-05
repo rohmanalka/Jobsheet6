d75e5fc57793a17b69383baed27d8ae726aab517
rohmanalka <rohmanalka06@gmail.com>
2023-10-05 09:11:58 +0700

    code pertama

--- Pemilihan2Percobaan1.java
+++ Pemilihan2Percobaan1.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan1 {
     public static void main(String[] args) {
         Scanner input21 = new Scanner(System.in);
-        System.out.println("Masukkan Tahun: ");
+        System.out.print("Masukkan Tahun: ");
         int tahun = input21.nextInt();
 
         if ((tahun % 4)== 0) {
             if ((tahun % 100) != 0)
                 System.out.println("Tahun Kabisat");
-        else 
+        
+        }else 
             System.out.println("Bukan Tahun Kabisat");
-        }
     }
 }
